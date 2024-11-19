ad6bd8727c220c65779454cfc74ee5217bae304f
tavakol5272 <148060212+tavakol5272@users.noreply.github.com>
2024-11-19 18:22:51 +0100

    Update models.py

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -28,26 +28,26 @@ class Player(BasePlayer):
     #this is the most important feature of this file. We can collect all the variables used on the html pages here
     
 #The Variables are structured on the base of pages
-    permission = models.StringField(label="if you are sure that you want to participate in this survey please type: ok ",blank=True,)
-    Gender = models.StringField(label="What is your gender?",
-        choices=["Male", "Female", "Non-binary", "prefer not to say"],
+    permission = models.StringField(label="if you are sure that you want to participate in this survey please type: OK ",blank=True,)
+    Gender = models.StringField(label="1- What is your gender? ",
+        choices=["Male", "Female", "Non-binary", "prefer not to say "],
         widget=widgets.RadioSelect,)
     
-    Age = models.IntegerField(max=110, min=1, )  
+    Age = models.IntegerField(label="2- How old are you? ", max=110, min=1, )  
 
-    Academic_status = models.StringField(
+    Academic_status = models.StringField(label="3- What is your level of education? ",
         choices=["Undergraduate","Diploma","Bachelor", "Master", "PHD", "Other"]
         , widget=widgets.RadioSelect,)
     
-    Marital_status =   models.StringField(label="What is your Marital Status?",
+    Marital_status =   models.StringField(label="4- What is your Marital Status? ",
         choices=["Married", "Single", "Have partner", "prefer not to say"]
         , widget=widgets.RadioSelect,)
     
-    Monthly_income = models.IntegerField(label="What is your net monthly income(Euro)?", max=20000, min=1, )
+    Monthly_income = models.IntegerField(label="5- What is your net monthly income(Euro)? ", max=20000, min=1, )
     
-    life_satisfaction_score= models.IntegerField(label="How satisfied are you with your overall life?(1-100)?", max=100, min=1, )
+    life_satisfaction_score= models.IntegerField(label="6- How satisfied are you with your overall life?(1-100)? <br>", max=100, min=1, )
     
-    Idea = models.StringField(label="What is your idea about this survey?",blank=True,)
+    Idea = models.StringField(label="7- What is your idea about this survey? ",blank=True,)
     
 
     
